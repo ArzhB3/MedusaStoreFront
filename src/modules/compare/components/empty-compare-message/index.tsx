9edbff56312a9e4144b97e2869101b6afe9bccfc
Arzh <arthur.pagelot@gmail.com>
2025-03-08 20:42:59 +0900

    Manage compare page lower than 2 products

---
+++ src/modules/compare/components/empty-compare-message/index.tsx
@@ -0,0 +1,28 @@
+import { Heading, Text } from "@medusajs/ui"
+
+import InteractiveLink from "@modules/common/components/interactive-link"
+
+const EmptyCompareMessage = () => {
+  return (
+    <div
+      className="py-48 px-2 flex flex-col justify-center items-start"
+      data-testid="empty-compare-message"
+    >
+      <Heading
+        level="h1"
+        className="flex flex-row text-3xl-regular gap-x-2 items-baseline"
+      >
+        Compare
+      </Heading>
+      <Text className="text-base-regular mt-4 mb-6 max-w-[32rem]">
+        Sorry, there are nothing to compare here. Please go back to the store
+        and add at least two products to compare.
+      </Text>
+      <div>
+        <InteractiveLink href="/store">Explore products</InteractiveLink>
+      </div>
+    </div>
+  )
+}
+
+export default EmptyCompareMessage
